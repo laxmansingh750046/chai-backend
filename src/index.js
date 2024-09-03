1df502182662abd47421e8e335e4b0e1d8a22ee7
laxman singh <laxmansingh790616@gmail.com>
2024-09-04 04:12:11 +0530

    server listner

--- src/index.js
+++ src/index.js
@@ -2,6 +2,14 @@
 
 import dotenv from 'dotenv';
 import connectDB from './db/index.js';
+import app from './app.js'
 
-
-connectDB();
+connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 8000,()=>{
+        console.log(`server is running at port ${process.env.PORT || 8000}`);
+    });
+})
+.catch((error)=>{
+    console.log('mongodb connection failed ',error);
+})
